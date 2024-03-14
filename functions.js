16801feed1383c6bf112d70a757675c5f36e72f2
Brunitop <68580588+Brunitop@users.noreply.github.com>
2024-03-14 10:13:20 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,6 +5,45 @@ var salariosPorCargo = {
     "Empleado": 300
 };
 
+const ispt_index = [1.92, 6.4, 10.88, 16, 17.92, 21.36, 23.52, 30, 32, 34, 35];
+
+function limiteISPT(salarioTotal){
+    if (salarioTotal > 0.01 && salarioTotal < 496.07){
+        return ispt_index[0];
+    }
+    else if(salarioTotal > 496.08 && salarioTotal < 4210.41){
+        return ispt_index[1];
+    }
+    else if(salarioTotal > 4210.42 && salarioTotal < 7399.42){
+        return ispt_index[2];
+    }
+    else if(salarioTotal > 7399.43 && salarioTotal < 8601.5){
+        return ispt_index[3];
+    }
+    else if(salarioTotal > 8601.51 && salarioTotal < 10298.35){
+        return ispt_index[4];
+    }
+    else if(salarioTotal > 10298.36 && salarioTotal < 20770.29){
+        return ispt_index[5];
+    }
+    else if(salarioTotal > 20,770.3 && salarioTotal < 32736.83){
+        return ispt_index[6];
+    }
+    else if(salarioTotal > 32736.84 && salarioTotal < 62500){
+        return ispt_index[7];
+    }
+    else if(salarioTotal > 62500.01 && salarioTotal < 83333.33){
+        return ispt_index[8];
+    }
+    else if(salarioTotal > 83333.34 && salarioTotal < 250000){
+        return ispt_index[9];
+    }
+    else if(salarioTotal > 250000.01){
+        return ispt_index[10];
+    }
+}
+
+
 function SalarioPorDia() {
     var inputSection = document.getElementById("data-input");
     inputSection.innerHTML = `
@@ -40,25 +79,31 @@ function SalarioPorDia() {
 }
 
 function calcularSalarioPorDia() {
-    var resultadoElement = document.getElementById("resultado");
+    var resultadoTotal = document.getElementById("salario-total");
+    var resultadoISPT = document.getElementById("salario-impuesto");
     var idEmpleado = document.getElementById("id-empleado").value;
     var horasTrabajadas = parseFloat(document.getElementById("horas-trabajadas").value);
     var diasTrabajados = parseFloat(document.getElementById("dias-trabajados").value);
     var tipoCargo = document.getElementById("tipo-cargo").value;
 
-    if (isNaN(horasTrabajadas) || isNaN(diasTrabajados) || horasTrabajadas <= 0 || diasTrabajados <= 0) {
-        resultadoElement.innerText = "Error: Las horas y días trabajados deben ser números positivos.";
-        return;
-    }
-
     var salarioPorHora = salariosPorCargo[tipoCargo];
-    if (salarioPorHora === undefined) {
-        resultadoElement.innerText = "Error: Tipo de cargo no válido.";
+    if (isNaN(horasTrabajadas) || isNaN(diasTrabajados) || horasTrabajadas <= 0 || diasTrabajados <= 0) {
+        resultadoTotal.innerText = "Error: Las horas y días trabajados deben ser números positivos.";
         return;
     }
+    else if (salarioPorHora === undefined) {
+        resultadoTotal.innerText = "Error: Tipo de cargo no válido.";
+        return;
+    }
+    else{
+        var salarioTotal = salarioPorHora * horasTrabajadas * diasTrabajados;
+        var lim_ispt = limiteISPT(salarioTotal);
+        var salarioISPT = salarioTotal - (salarioTotal * lim_ispt)/100;
 
-    var salarioTotal = salarioPorHora * horasTrabajadas * diasTrabajados;
-    resultadoElement.innerText = "El empleado " + idEmpleado + " ganó $" + salarioTotal + " esta semana.";
+        console.log("El empleado " + idEmpleado + " ganó $" + salarioTotal + " esta semana.")
+        resultadoTotal.innerText = "El empleado " + idEmpleado + " ganó $" + salarioTotal + " esta semana."
+        resultadoISPT.innerText = "Salario con impuesto (ISPT): " + salarioISPT;
+    }
 }
 
 function SalarioPorMes() {
@@ -88,18 +133,25 @@ function SalarioPorMes() {
 }
 
 function calcularSalarioPorMes() {
-    var resultadoElement = document.getElementById("resultado");
+    var resultadoTotal = document.getElementById("salario-total");
+    var resultadoISPT = document.getElementById("salario-impuesto");
     var idEmpleado = document.getElementById("id-empleado").value;
     var tipoCargo = document.getElementById("tipo-cargo").value;
 
     var salarioPorMes = salariosPorCargo[tipoCargo];
     if (salarioPorMes === undefined) {
-        resultadoElement.innerText = "Error: Tipo de cargo no válido.";
+        resultadoTotal.innerText = "Error: Tipo de cargo no válido.";
         return;
     }
+    else{
+        var salarioTotal = salarioPorMes * 30;
+        var lim_ispt = limiteISPT(salarioTotal);
+        var salarioISPT = salarioTotal - (salarioTotal * lim_ispt)/100;
 
-    var salario = salarioPorMes * 30;
-    resultadoElement.innerText = "El empleado " + idEmpleado + " gana $" + salario + " mensuales.";
+        resultadoTotal.innerText = "El empleado " + idEmpleado + " ganó $" + salarioTotal + " esta semana."
+        resultadoISPT.innerText = "Salario con impuesto (ISPT): " + salarioISPT;
+        //resultadoElement.innerText = "El empleado " + idEmpleado + " gana $" + salario + " mensuales.";
+    }
 }
 
 function SalarioPorHora() {
@@ -133,22 +185,29 @@ function SalarioPorHora() {
 }
 
 function calcularSalarioPorHora() {
-    var resultadoElement = document.getElementById("resultado");
+    var resultadoTotal = document.getElementById("salario-total");
+    var resultadoISPT = document.getElementById("salario-impuesto");
     var idEmpleado = document.getElementById("id-empleado").value;
     var horasTrabajadas = parseFloat(document.getElementById("horas-trabajadas").value);
     var tipoCargo = document.getElementById("tipo-cargo").value;
 
-    if (isNaN(horasTrabajadas) || horasTrabajadas <= 0) {
-        resultadoElement.innerText = "Error: Las horas trabajadas deben ser un número positivo.";
-        return;
-    }
-
     var salarioPorHora = salariosPorCargo[tipoCargo];
-    if (salarioPorHora === undefined) {
-        resultadoElement.innerText = "Error: Tipo de cargo no válido.";
+
+    if (isNaN(horasTrabajadas) || horasTrabajadas <= 0) {
+        resultadoTotal.innerText = "Error: Las horas trabajadas deben ser un número positivo.";
         return;
     }
+    else if (salarioPorHora === undefined) {
+        resultadoTotal.innerText = "Error: Tipo de cargo no válido.";
+        return;
+    }
+    else{
+        var salarioTotal = salarioPorHora * horasTrabajadas;
+        var lim_ispt = limiteISPT(salarioTotal);
+        var salarioISPT = salarioTotal - (salarioTotal * lim_ispt)/100;
 
-    var salarioTotal = salarioPorHora * horasTrabajadas;
-    resultadoElement.innerText = "El empleado " + idEmpleado + " ganó $" + salarioTotal + " esta semana.";
+        resultadoTotal.innerText = "El empleado " + idEmpleado + " ganó $" + salarioTotal + " esta semana."
+        resultadoISPT.innerText = "Salario con impuesto (ISPT): " + salarioISPT;
+        //resultadoElement.innerText = "El empleado " + idEmpleado + " ganó $" + salarioTotal + " esta semana.";
+    }
 }
